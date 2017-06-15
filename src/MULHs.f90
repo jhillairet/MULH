cfeb718e1dc4f177921516b231d845ba06c43bbf
AdrienPlacais <adrien.placais@centrale-marseille.fr>
2017-06-15 10:46:38 +0200

    All MULHS.f90 parameters must now be logged in config.mulh

--- src/MULHs.f90
+++ src/MULHs.f90
@@ -61,69 +61,70 @@ implicit none
 ! 
 ! NOTE = 2-7, 10, 11 and 12 can take a very long time so it is recommended to run them remotely (in a server)
 !
-atype = 4
+open(unit=1, file='../config.mulh',status='old')
+read(1,*) atype
 
 !!!!!!!!! Geometry !!!!!!!!!
-b = 70			! Height of the waveguide in mm
-a = 8			! Width of the waveguide in mm
-lw = 150		! Depth/Length of the waveguide in mm. If changed make sure there are still enough cells per wavelength
-lwu = 160		! Upper waveguide length (if atype==2)
-
-NOC_lambda = 20		! Number of Cells per wavelength
-NOC_lambdau = 50	! Upper number of Cells per wavelength (for atype==3)
- CLfactor = 0.95	! Factor to make sure Courant-Levy condition is obeyed. Decrease CLfactor for smaller time step
-Prec = 0.1		! Threshold precision (dB)
+read(1,*) b		! Height of the waveguide in mm
+read(1,*) a		! Width of the waveguide in mm
+read(1,*) lw		! Depth/Length of the waveguide in mm. If changed make sure there are still enough cells per wavelength
+read(1,*) lwu		! Upper waveguide length (if atype==2)
+read(1,*) NOC_lambda	! Number of Cells per wavelength
+read(1,*) NOC_lambdau	! Upper number of Cells per wavelength (for atype==3)
+read(1,*) CLfactor	! Factor to make sure Courant-Levy condition is obeyed. Decrease CLfactor for smaller time step
+read(1,*) Prec		! Threshold precision (dB)
 
 eta = mu_0 * c
 !!!!!!!!! Input Wave/Field solver !!!!!!!!!
-f_I = 3.7e9	! Frequency of input wave in Hz
+read(1,*) f_I	! Frequency of input wave in Hz
 lambda = c/f_I	! Wavelength
-Pl0 = 1.0e4	! Lower power limit (watts)
-Pu0 = 1.0e6
-!Pu0 = anint( ((10e5)**2) * (a/1000.) * (b/1000.) * sqrt(1-(lambda/(2*(b/1000.)))**2) / (4*eta) )      ! Upper power limit (watts)
+read(1,*) Pl0	! Lower power limit (watts)
+read(1,*) Pu0	! Upper power limit (watts)
 
-fields = 1	! Field solver. =1 FDTD, =2 analytic TE10 mode, =3 exported from other solver(need NOC_PML=1 w/ fields=3)
-ramp = 1	! Increase fields slowly over ramp periods, integer
-NOC_PML = 1	! Number Of Cells in PML, integer
-R_max = 1e-10	! Reflection error for normally incident wave (as a fraction)
-m_PML = 3	! PML grading order, integer
+read(1,*) fields	! Field solver. =1 FDTD, =2 analytic TE10 mode, =3 exported from other solver(need NOC_PML=1 w/ fields=3)
+read(1,*) ramp		! Increase fields slowly over ramp periods, integer
+read(1,*) NOC_PML	! Number Of Cells in PML, integer
+read(1,*) R_max		! Reflection error for normally incident wave (as a fraction)
+read(1,*) m_PML		! PML grading order, integer
 
 !!!!!!!!!! Static DC magnetic field (poloidal + toroidal). Gauss format, e.g. 2/10000. Minimum field allowed = 1G!!!!!!!!!
-sBx = 1.0e-3		! Toroidal magnetic field at plasma center (T)
-sBxu = 1.0e-2		! Upper toroidal magnetic field at plasma center (T) (for atype==4)
-sBy = 0.		! Poloidal magnetic field in waveguide (T)
-sByu = 0.		! Upper poloidal magnetic field in waveguide (T)
-sBz = 0.		! Radial magnetic field in waveguide (T)
-sBzu = 0.		! Upper radial magnetic field in waveguide (T)
+read(1,*) sBx		! Toroidal magnetic field at plasma center (T)
+read(1,*) sBxu		! Upper toroidal magnetic field at plasma center (T) (for atype==4)
+read(1,*) sBy		! Poloidal magnetic field in waveguide (T)
+read(1,*) sByu		! Upper poloidal magnetic field in waveguide (T)
+read(1,*) sBz		! Radial magnetic field in waveguide (T)
+read(1,*) sBzu		! Upper radial magnetic field in waveguide (T)
 
 !!!!!!!!!! Particles !!!!!!!!!
-Np = 100	! Number of primary particles (has to be even, preferably multiples of 16)
-vth = 5		! Initial energy of seed electrons (eV), integer
-vra = sqrt(2.)	! Ratio of vth_perpendicular to vth_parallel (sqrt(2) for isotropic)
-px_i = 3	! Position of seed e 1=Side walls 2=4 planes parallel to side walls 3= Randomly scattered in centered region
-fmax = 4	! vth*fMax is the largest velocity represented for the sample array fSample
-nsamplev = 250	! # of sample fraction values for creating a Maxwellian velocity distribution function
-launch = 3	! Launch method 1=each at a different phase [0,360] 2=range of phases every 5 or 10 degrees [0,360] 3=random
+read(1,*) Np		! Number of primary particles (has to be even, preferably multiples of 16)
+read(1,*) vth		! Initial energy of seed electrons (eV), integer
+read(1,*) vra		! Ratio of vth_perpendicular to vth_parallel (sqrt(2) for isotropic)
+read(1,*) px_i		! Position of seed e 1=Side walls 2=4 planes parallel to side walls 3= Randomly scattered in centered region
+read(1,*) fmax		! vth*fMax is the largest velocity represented for the sample array fSample
+read(1,*) nsamplev	! # of sample fraction values for creating a Maxwellian velocity distribution function
+read(1,*) launch	! Launch method 1=each at a different phase [0,360] 2=range of phases every 5 or 10 degrees [0,360] 3=random
 
 !!!!!!!!!! Particle-Wall interaction !!!!!!!!!
-seec = 1	! SEE model, =1 Modified Vaughan, =2 Furman & Pivi, =3 Cheng w/ de Lara Re/Rr, =4 FEST3Dish?
-ReRr = 1	! Contributions from elastically (Re) and rediffused (Rr) electrons. =1 de Lara, =2 CERN LHC report
-E1 = 35		! First crossover
-Emax = 165	! Emax(delta=max,theta=0) in eV
-delta_b = 0.5	! Yield below E_0
-deltamax = 2.3	! Maximum secondary electron yield (at Emax) for normal incidence (theta=0)
-p_n = 1		! p_n phenomelogical parameter in Furman & Pivi, ==2 in Cheng
-kse = 1		! Energy Roughness factor, [0,2], 0=rough,2=smooth
-ks = 1		! Angle Roughness factor, [0,2], 0=rough,2=smooth
-Eom =1		! Av energy of Maxwellian distribution of secondary electrons emitted (eV)
-Ns = 4		! Number of secondary electrons simulated (in multiples of Np)
-a_lara = 7.5e-3	! Material dependent coeff for elastic contribution in de Lara's paper
-z_lara = 29	! Atomic number of coating material in Lara's fit
-mat = 'copper'	! Material
+read(1,*) seec		! SEE model, =1 Modified Vaughan, =2 Furman & Pivi, =3 Cheng w/ de Lara Re/Rr, =4 FEST3Dish?
+read(1,*) ReRr		! Contributions from elastically (Re) and rediffused (Rr) electrons. =1 de Lara, =2 CERN LHC report
+read(1,*) E1		! First crossover
+read(1,*) Emax		! Emax(delta=max,theta=0) in eV
+read(1,*) delta_b	! Yield below E_0
+read(1,*) deltamax	! Maximum secondary electron yield (at Emax) for normal incidence (theta=0)
+read(1,*) p_n		! p_n phenomelogical parameter in Furman & Pivi, ==2 in Cheng
+read(1,*) kse		! Energy Roughness factor, [0,2], 0=rough,2=smooth
+read(1,*) ks		! Angle Roughness factor, [0,2], 0=rough,2=smooth
+read(1,*) Eom		! Av energy of Maxwellian distribution of secondary electrons emitted (eV)
+read(1,*) Ns		! Number of secondary electrons simulated (in multiples of Np)
+read(1,*) a_lara	! Material dependent coeff for elastic contribution in de Lara's paper
+read(1,*) z_lara	! Atomic number of coating material in Lara's fit
+read(1,*) mat		! Material
 
 ! Save outputs
-psave = 0	! Save particle position and velocity every psave iteration, =0 for not saving
+read(1,*) psave		! Save particle position and velocity every psave iteration, =0 for not saving
 
+
+close(unit=1)
 !
 !
 !		END OF USER INPUTS
